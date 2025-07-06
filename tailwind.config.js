4fb975a1883392de78a52f81d9d38cc0182a542c
Andi <maarzsyena@gmail.com>
2025-07-06 15:29:19 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ini memberitahu Tailwind untuk memindai semua file .js, .jsx, dll. di dalam folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
